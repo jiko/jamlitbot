76a684899a036a819a0d420cb4184830ddf7dfec
James Koppen <jk@jameskoppen>
2013-01-06 08:08:54 -0600

    Bumped up the font-size

--- app.py
+++ app.py
@@ -11,7 +11,7 @@ def hello(r="hf9831h1rgfewuifgasjkbzxg1e"):
    if (markov.mapping=={}):
 	   markov.buildMapping(markov.wordlist(filename),markovLength)
    sentence = markov.genSentence(markovLength, r.lower())
-   page = "<!DOCTYPE html><html><header><title>JAMLITBOT</title><style type='text/css'>a {color:black;text-decoration:none}</style></header><body>"
+   page = "<!DOCTYPE html><html><header><title>JAMLITBOT</title><style type='text/css'>a {color:black;text-decoration:none;font-size:16pt;}</style></header><body>"
    for word in sentence.split():
 		page += "<a href='"+word.translate(string.maketrans("",""),string.punctuation)+"'>"+word+"</a> "
    page += "<br><br><a href='/'>reload</a></body></html>"
